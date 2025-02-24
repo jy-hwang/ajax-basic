0a2c6d6eea5346427eab04309dda4832699a7867
jyhwang <stratojy@gmail.com>
2025-02-24 20:28:17 +0900

    #4 - JSON 을 활용해 서블릿 구현하기
    1. Json 을 활용한 서블릿 구현하기
    2. Json 형식으로 append에 넣기 어려울때는 바깥에서 주석으로 한번 만들어놓고 복사하면 편하게 넣을 수 있다.

---
+++ src/main/java/user/UserSearchServlet.java
@@ -0,0 +1,49 @@
+package user;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/userSearchServlet")
+public class UserSearchServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    request.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html;charset=UTF-8");
+    String userName = request.getParameter("userName");
+    response.getWriter().write(getJson(userName));
+  }
+
+
+  public String getJson(String userName) {
+
+    if (userName == null) {
+      userName = "";
+    }
+
+    StringBuffer result = new StringBuffer("");
+    // "{"result" : ["
+    result.append("{\"result\" : [");
+    UserDAO userDAO = new UserDAO();
+
+    ArrayList<User> userList = userDAO.search(userName);
+    // [{"value" : " + userList.get(i).getUserName() + ""}]
+    for (int i = 0; i < userList.size(); i++) {
+      result.append("[{\"name\" : \"" + userList.get(i).getUserName() + "\"},");
+      result.append("{\"age\" : \"" + userList.get(i).getUserAge() + "\"},");
+      result.append("{\"gender\" : \"" + userList.get(i).getUserGender() + "\"},");
+      result.append("{\"email\" : \"" + userList.get(i).getUserEmail() + "\"}],");
+    }
+
+    result.append("]}");
+    System.out.println(result.toString());
+    return result.toString();
+  }
+
+}
